2fccfb01d73771b98c0a63f0267f2bfe1a8fca8f
Ayush kumawat <ayushkumawat@Ayushs-MacBook-Air.local>
2024-10-25 17:52:53 +0530

    yo

--- src/app.js
+++ src/app.js
@@ -4,15 +4,16 @@ const app=express();
 
 app.listen(3000);
 
-app.get("/:user",(req,res)=>{
-    const params=req.params;
-    const query=req.query;
-    res.json({params:params,
-        query:query}
-    );});
+app.get("/mahika",(req,res,next)=> { console.log("mahika");  next();  res.send("mahika"); },(req,res)=> res.send("mahika 2nd")); // next() is given by ExpressJS
 
-  
-   
+app.post("/mahika",(req,res)=> res.send("mahika"));
+
+app.get("/mahika/ganduka",(req,res)=> res.send("ganduka"));
+
+
+
+
+
 
 
 
