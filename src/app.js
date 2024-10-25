e7b3422b8899fe31f6d2947b88aea262ec24ec80
Ayush kumawat <ayushkumawat@Ayushs-MacBook-Air.local>
2024-10-25 09:15:06 +0530

    mahak

--- src/app.js
+++ src/app.js
@@ -4,13 +4,15 @@ const app=express();
 
 app.listen(3000);
 
-app.get("testcase2/:username",(req,res)=>res.send("testcase2"));
+app.get("/testcase2/:username",(req,res)=>res.send("testcase2"));
 
 app.use("/testcase1",(req,res)=> res.send("testcase1"));
 
 app.use("/",(req,res)=> res.send("mahika"));
 
-app.post("testcase2/:username",(req,res)=>res.send("testcase2"));
+app.post("/testcase2/:username",(req,res)=>res.send("testcase2"));
+
+app.post("/testcase2",(req,res)=>res.send("testcase2"));
 
 
 
