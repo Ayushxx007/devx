97ef501b52401430a57da52fbb433f3c3b41c42a
Ayush kumawat <ayushkumawat@Ayushs-MacBook-Air.local>
2024-10-25 09:19:28 +0530

    sdsfds

--- src/app.js
+++ src/app.js
@@ -14,6 +14,16 @@ app.post("/testcase2/:username",(req,res)=>res.send("testcase2"));
 
 app.post("/testcase2",(req,res)=>res.send("testcase2"));
 
+app.get("/testcase2/:username",(req,res)=>res.send("testcase2"));
+
+app.use("/testcase1",(req,res)=> res.send("testcase1"));
+
+app.use("/",(req,res)=> res.send("mahika"));
+
+app.post("/testcase2/:username",(req,res)=>res.send("testcase2"));
+
+app.post("/testcase2",(req,res)=>res.send("testcase2"));
+
 
 
 
