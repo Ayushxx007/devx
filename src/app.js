d79a00a1b0ff96e36b90f7c315a8f3731d07dbdf
Ayush kumawat <ayushkumawat@Ayushs-MacBook-Air.local>
2024-10-26 17:37:36 +0530

    texas

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const {User}=require("./models/user.js");
 app.use(express.json()) // MiddleWare
 
 
-app.post("/signup", async (req,res)=>{
+app.post("/signup", async (req,res)=>{             // adding data of a user to database
            // express converted req as an object
 
    
@@ -27,7 +27,7 @@ app.post("/signup", async (req,res)=>{
         }
 });
 
-app.get("/user", async(req,res)=>{
+app.get("/user", async(req,res)=>{     // get al perticular user from database by emailId
 
     const email= req.body.emailId;
 
@@ -70,27 +70,37 @@ app.get("/feed", async(req,res)=>{     // Feed api-- get all the users from data
 
 });    
 
-app.delete("/user",async(req,res)=>{
+app.delete("/user",async(req,res)=>{           // Deleting user from database
 
-    const userId=req.body.userId;
+const userId=req.body.userId;
     
 
-    try{
-        const user=await User.findByIdAndDelete({_id:userId});
+try{
+    const user=await User.findByIdAndDelete({_id:userId});
 
-        res.send("deleted successfully");
+    res.send("deleted successfully");
 
+}catch(err){
 
-
-    }catch(err){
-
-        res.status(400).send("something went wrong");
+res.status(400).send("something went wrong");
 
     }
-
-
 });
 
+app.patch("/user",async(req,res)=>{       // updating data  in database
+
+    const data=req.body;
+    const userId=req.body.userId;
+
+ try{
+        await User.findByIdAndUpdate({_id:userId},data);
+        res.send("updated successfully");
+
+ }catch(err){
+        res.status(400).send("something went wrong");
+ }
+
+});
 
 
 
